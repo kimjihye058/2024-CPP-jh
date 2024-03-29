4792010addd467b060f9635b1b299fdac1ed4ee0
kimjihye058 <s2321@e-mirim.hs.kr>
2024-03-29 09:08:05 +0900

    배열과 포인터

--- Project1/pointer/배열과 포인터.cpp
+++ Project1/pointer/배열과 포인터.cpp
@@ -5,11 +5,15 @@ int main(void)
 	int arr[4] = { 10, 20, 30, 40 };
 	char str[4];
 
-	// 두 코드의 결과는 같다.
-	printf("%d %d\n", *(&arr[0]), *(&arr[1]));
-	printf("%d %d\n", *(arr), *(arr + 1));
+	int* pi = arr;
+	char* pc = str; 
 
-	// &를 쓰는 참조 연산이라고 하고 *을 쓰는 연산은 역참조 연산이라고 한다.
+	// 두 코드의 결과는 같다.
+	printf("%d %d\n", (&arr[0]), (&arr[1]));	// 4차이
+	printf("%d %d\n\n", (arr), (arr + 1));		
+
+	printf("%d %d\n", (pi), (pi + 1));			// 4차이
+	printf("%d %d\n", (pc), (pc + 1));			// 1차이
 	
 	return 0;
 }
