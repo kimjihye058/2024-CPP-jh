cf17ee94e24ccf20f1480ad03b8204cdffea31ed
kimjihye058 <s2321@e-mirim.hs.kr>
2024-04-08 14:40:52 +0900

    구조체는 default가 public이다.

---
+++ Project1/03-01 구조체와 클래스.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// 구조체는 default가 public이다.
+struct Student {
+	int hakbun_;
+	string name_;
+};
+
+
+void main(void) {
+	struct Student juyoung;
+	juyoung.hakbun_ = 2115;
+	juyoung.name_ = "윤주영";
+}
