292513ca71b88507d527406d05be1dffe5119d68
kimjihye058 <s2321@e-mirim.hs.kr>
2024-05-27 14:58:55 +0900

    05-01 상속 기본

---
+++ Project1/05-01 상속 기본.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Person {
+public:
+	Person(string name, int age)
+		: name_(name), age_(age) {}
+	void show_person() {
+		cout << name_ << "의 나이는 " << age_ << "입니다." << endl;
+	}
+
+private:
+	string name_;
+	int age_;
+};
+
+// Person의 자식(파생) 클래스
+class Student : public Person {
+public:
+	// 부모(기본) 클래스의 생성자가 먼저 호출되어야 한다.
+	Student(int id, string name, int age) : Person(name, age), id_(id)
+	{
+
+	}
+	void show_person(void)
+	{
+		cout << "학번" << id_ << endl;
+		Person::show_person();
+	}
+private:
+	int id_;
+};
+
+int main(void)
+{
+	// Student 객체 Kim을 동적 할당으로 생성
+	Student* Kim = new Student(2104, "Kimjihye", 18);
+	Kim->show_person();
+
+	// 메모리 해제
+	delete Kim;
+
+	return 0;
+}
