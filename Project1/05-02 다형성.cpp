93e2ff04569ddf68646247e01658917940302610
kimjihye058 <s2321@e-mirim.hs.kr>
2024-08-16 10:21:16 +0900

    동적할당 복습

---
+++ Project1/05-02 다형성.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal {
+public:		// 멤버 함수
+	void bark() {
+
+	}
+
+	void sleep() {
+
+	}
+
+	void eat() {
+
+	}
+
+private:		// 멤버 변수
+	string name;
+	unsigned int age;		// 음의 부호가 없는 int
+
+};
+
+void main(void)
+{
+	Animal* ani = new Animal();		// 동적할당
+}
