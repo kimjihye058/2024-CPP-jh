9e1ed10258badba40b48f4024a013dee0a3d42f4
kimjihye058 <s2321@e-mirim.hs.kr>
2024-08-19 14:47:19 +0900

    아주 무난한 다형성

--- Project1/05-02 다형성.cpp
+++ Project1/05-02 다형성.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 class Animal {
 public:		// 멤버 함수
+	Animal() {
+
+	}
+
 	Animal(string name, unsigned int age) 
 		: name_(name), age_(age)
 	{
-		cout << "이름" << name_ << endl;
-		cout << "나이" << age_ << endl;
+		cout << "이름 " << name_ << endl;
+		cout << "나이 " << age_ << endl;
 	}
+
 	void bark() {
 		cout << "잘 짖는다." << endl;
 	}
@@ -31,7 +36,25 @@ private:		// 멤버 변수
 
 class Human : public Animal {
 public:
-	
+	// 부모(Animal) 생성자가 먼저 호출된다.
+	Human(string name, unsigned int age, bool right) 
+		: Animal(name, age), right_(right)
+	{
+		cout << "인권존재여부 " << right_ << endl;
+	}
+
+	void bark() {
+		cout << "톡톡" << endl;
+	}
+
+	void sleep() {
+		cout << "쿨쿨" << endl;
+	}
+
+	void eat() {
+		cout << "얌얌" << endl;
+	}
+
 private:
 	bool right_;
 };
@@ -39,11 +62,17 @@ private:
 void main(void)
 {
 	Animal* ani = new Animal("정민레이디", 18);		// 동적할당
-
+	
 	ani->bark();
 	ani->sleep();
 	ani->eat();
 
-	delete ani;		// delete
+	Human* hum = new Human("지우맨", 18, true);		// 동적할당
+	hum->bark();
+	hum->sleep();
+	hum->eat();
+
+	delete hum;		// 할당 해제
+	delete ani;		// 할당 해제
 
 }
