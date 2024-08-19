5ecc9b4c76545d961c9a1ce3687a169f5473e6c9
kimjihye058 <s2321@e-mirim.hs.kr>
2024-08-19 13:47:05 +0900

    동적할당 복습2

--- Project1/05-02 다형성.cpp
+++ Project1/05-02 다형성.cpp
@@ -6,15 +6,15 @@ using namespace std;
 class Animal {
 public:		// 멤버 함수
 	void bark() {
-
+		cout << "잘 짖는다." << endl;
 	}
 
 	void sleep() {
-
+		cout << "잘 잔다." << endl;
 	}
 
 	void eat() {
-
+		cout << "잘 먹는다." << endl;
 	}
 
 private:		// 멤버 변수
@@ -26,4 +26,10 @@ private:		// 멤버 변수
 void main(void)
 {
 	Animal* ani = new Animal();		// 동적할당
+
+	ani->bark();
+	ani->sleep();
+	ani->eat();
+
+	delete ani;		// delete
 }
