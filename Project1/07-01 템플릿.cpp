6f7d60fb68b3a7ff125640566f7bb12e497db062
kimjihye058 <s2321@e-mirim.hs.kr>
2024-08-30 09:16:20 +0900

    07-01 템플릿 적용 후

--- Project1/07-01 템플릿.cpp
+++ Project1/07-01 템플릿.cpp
@@ -3,14 +3,12 @@
 
 using namespace std;
 
-// 함수 오버로딩 - 번거롭고 유연하지 않음
-int sum(int a, int b) { return a + b; }
-float sum(float a, float b) { return a + b; }
-string sum(string a, string b) { return a + b; }
+template <typename T>
+T sum(T a, T b) { return a + b; }
 
 void main(void) 
 {
-	cout << sum(1, 2) << endl;
-	cout << sum(1.f, 2.f) << endl;
-	cout << sum("1", "2") << endl;
+	cout << sum<int>(1, 2) << endl;
+	cout << sum<float>(1.f, 2.f) << endl;
+	cout << sum<string>("1", "2") << endl;
 }
