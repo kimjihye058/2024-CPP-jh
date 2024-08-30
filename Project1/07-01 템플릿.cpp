3994c0ef07f6575d4384e0b6ba66d589a7bccfe9
kimjihye058 <s2321@e-mirim.hs.kr>
2024-08-30 09:12:13 +0900

    07-01 템플릿 적용 전 (함수 오버로딩)

---
+++ Project1/07-01 템플릿.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 함수 오버로딩 - 번거롭고 유연하지 않음
+int sum(int a, int b) { return a + b; }
+float sum(float a, float b) { return a + b; }
+string sum(string a, string b) { return a + b; }
+
+void main(void) 
+{
+	cout << sum(1, 2) << endl;
+	cout << sum(1.f, 2.f) << endl;
+	cout << sum("1", "2") << endl;
+}
