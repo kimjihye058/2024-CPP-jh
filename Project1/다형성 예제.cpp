4e112cd65d59d325502dd69154048a87c2a779ff
kimjihye058 <s2321@e-mirim.hs.kr>
2024-09-02 15:05:44 +0900

    다형성예제1

--- Project1/다형성 예제.cpp
+++ Project1/다형성 예제.cpp
@@ -3,117 +3,87 @@
 
 using namespace std;
 
-// 부모 클래스 Vehicle (탈 것)
-class Vehicle {
-protected:
-    int numWheels;      // 바퀴 개수
-    string color;       // 색깔
-    int maxPassengers;  // 탑승 가능 인원
+class Clothes {
+public:		// 멤버함수
+	Clothes(string name, int price, int making_time, int beauty) 
+		: name_(name), price_(price), making_time_(making_time), beauty_(beauty)
+	{}
 
-public:
-    Vehicle(int wheels, string color, int passengers)
-        : numWheels(wheels), color(color), maxPassengers(passengers) {}
+	virtual void show()
+	{
+		cout << "이름: " << name_ << endl;
+		cout << "가격: " << price_ << endl;
+		cout << "만드는 시간: " << making_time_ << endl;
+		cout << "아름다움: " << beauty_ << endl;
+	}
 
-    // 탈 것의 기본 정보를 출력하는 메소드
-    void showInfo() {
-        cout << "바퀴 개수: " << numWheels << "개" << endl;
-        cout << "색깔: " << color << "색" << endl;
-        cout << "최대 탑승 가능 인원: " << maxPassengers << "명" << endl;
-    }
+private:	// 멤버변수
+	string name_;
+	int price_;
+	int making_time_;
+	int beauty_;
+
 };
 
-// 자식 클래스 Car (자동차)
-class Car : public Vehicle {
+class Hanbok : public Clothes {
+public:
+	Hanbok(string name, int price, int making_time, int beauty, int norigae, int jugori)
+		: Clothes(name, price, making_time, beauty), norigae_(norigae), jugori_(jugori) 
+	{}
+
+	void show()
+	{
+		Clothes::show();
+		cout << "노리개: " << norigae_ << endl;
+		cout << "저고리: " << jugori_ << endl;
+	}
+
 private:
-    string fuelType;  // 연료 종류 (가솔린, 디젤 등)
-
-public:
-    Car(string color, string fuelType)
-        : Vehicle(4, color, 5), fuelType(fuelType) {}
-
-    // 자동차의 특성을 출력하는 메소드
-    void showCarInfo() {
-        cout << "탈 것은 자동차 입니다." << endl;
-        showInfo();
-        cout << "이 자동차는 " << fuelType << " 연료를 사용합니다." << endl;
-    }
-
-    void showCar() {
-        cout << "자동차 부릉 부릉~" << endl;
-    }
+	int norigae_;			// 노리개
+	int jugori_;			// 저고리
 };
 
-// 자식 클래스 Motorcycle (오토바이)
-class Motorcycle : public Vehicle {
+class Kimono : public Clothes {
+public:
+	Kimono(string name, int price, int making_time, int beauty, int belt)
+		: Clothes(name, price, making_time, beauty), belt_(belt) {}
+
+	void show()
+	{
+		Clothes::show();
+		cout << "오비: " << belt_ << endl;
+	}
+
 private:
-    bool hasSidecar;  // 사이드카 유무
+	int belt_;				// 오비
 
-public:
-    Motorcycle(string color, bool sidecar)
-        : Vehicle(2, color, 2), hasSidecar(sidecar) {}
-
-    // 오토바이의 특성을 출력하는 메소드
-    void showMotorcycleInfo() {
-        cout << "탈 것은 오토바이 입니다." << endl;
-        showInfo();
-        if (hasSidecar) {
-            cout << "이 오토바이는 사이드카가 있습니다." << endl;
-        }
-        else {
-            cout << "이 오토바이는 사이드카가 없습니다." << endl;
-        }
-    }
-
-    void showMotorcycle() {
-        cout << "오토바이 부르릉~" << endl;
-    }
 };
 
-// 자식 클래스 Bicycle (자전거)
-class Bicycle : public Vehicle {
+class Chipao : public Clothes {
+public:
+	Chipao(string name, int price, int making_time, int beauty, int embroidery)
+		: Clothes(name, price, making_time, beauty), embroidery_(embroidery) {}
+
+	void show()
+	{
+		Clothes::show();
+		cout << "자수: " << embroidery_ << endl;
+	}
+
 private:
-    bool hasGear;  // 기어 유무
-
-public:
-    Bicycle(string color, bool gear)
-        : Vehicle(2, color, 1), hasGear(gear) {}
-
-    // 자전거의 특성을 출력하는 메소드
-    void showBicycleInfo() {
-        cout << "탈 것은 자전거 입니다." << endl;
-        showInfo();
-        if (hasGear) {
-            cout << "이 자전거는 기어가 있습니다." << endl;
-        }
-        else {
-            cout << "이 자전거는 기어가 없습니다." << endl;
-        }
-    }
-
-    void showBicycle() {
-        cout << "자전거 따르릉~" << endl;
-    }
+	int embroidery_;		// 자수
 };
 
-int main() {
-    // Car 객체 생성 및 정보 출력
-    Car car("빨강", "가솔린");
-    car.showCarInfo();
-    car.showCar();
+void main(void) 
+{
+	Clothes* player = new Hanbok("곤룡포", 100, 10, 9999, 0, 0);
+	Clothes* friendy = new Kimono("나마에와", 10, 1, 9, 1);
+	
+	player->show();
+	cout << endl;
+	friendy->show();
 
-    cout << endl;
+	delete friendy;
+	delete player;
 
-    // Motorcycle 객체 생성 및 정보 출력
-    Motorcycle motorcycle("검정", true);
-    motorcycle.showMotorcycleInfo();
-    motorcycle.showMotorcycle();
-
-    cout << endl;
-
-    // Bicycle 객체 생성 및 정보 출력
-    Bicycle bicycle("파랑", false);
-    bicycle.showBicycleInfo();
-    bicycle.showBicycle();
-
-    return 0;
 }
