4ce23d40800fd222e8f62ff1497e1f871df98b95
kimjihye058 <s2321@e-mirim.hs.kr>
2024-09-27 09:41:18 +0900

    09-01 enum 적용

--- Project1/09-01 enum.cpp
+++ Project1/09-01 enum.cpp
@@ -2,21 +2,27 @@
 
 using namespace std;
 
-void main(void)
-{
-	// 0 : 한식, 1: 일식, 2 : 중식
-	int style = 0;
+enum FoodStyle {
+    KOREAN,		// 0
+    JAPANESE,	// 1
+    CHINESE		// 2
+};
 
-	switch (style)
-	{
-	case 0:
-		cout << "한식" << endl;
-		break;
-	case 1:
-		cout << "일식" << end;
-		break;
-	case 2:
-		cout << "중식" << endl;
-		break;
-	}
+int main(void)
+{
+    // 0 : 한식, 1 : 일식, 2 : 중식
+    Style style = KOREAN;
+
+    switch (style)
+    {
+    case KOREAN:
+        cout << "한식" << endl;
+        break;
+    case JAPANESE:
+        cout << "일식" << endl;
+        break;
+    case CHINESE:
+        cout << "중식" << endl;
+        break;
+    }
 }
