8827c9004623dbf2d43aa78998fccde32a77917c
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 13:42:58 +0900

    09-03 auto 적용 전

---
+++ Project1/09-03 auto.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void main(void)
+{
+	int a = 40;
+	cout << a << endl;
+	
+	vector<string> v = { "허니지혜", "심정민 lady", "윤주영 베이비"};
+	string b = v[2];
+	cout << v[2] << endl;
+
+}
