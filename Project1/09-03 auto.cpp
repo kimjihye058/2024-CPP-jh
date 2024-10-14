cfe31b42044e765bcc35dbeef0711589a06da344
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 13:48:17 +0900

    09-03 auto 적용 후

--- Project1/09-03 auto.cpp
+++ Project1/09-03 auto.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 void main(void)
 {
-	int a = 40;
+	auto a = 40;
 	cout << a << endl;
 	
 	vector<string> v = { "허니지혜", "심정민 lady", "윤주영 베이비"};
-	string b = v[2];
+	auto b = v[2];
 	cout << v[2] << endl;
 
 }
