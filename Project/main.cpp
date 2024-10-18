88d9e318c97986a6445f3abaf96beaa6da893cb2
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-18 09:56:55 +0900

    [pj] 상수 처리방법1

---
+++ Project/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+#define WINDOW_WIDTH	1200
+#define WINDOW_HEIGHT	800
+
+void main(void)
+{
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Project");
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		RectangleShape player;
+		player.setSize(Vector2f(200.f, 200.f));
+		player.setPosition((1200-200)/2, (800-200)/2);
+		player.setFillColor(Color::Magenta);
+
+		window.clear(Color::White);
+		window.draw(player);
+		window.display();
+	}
+}
