fac938dca736f1032a47ec9fabc086583a8fe07c
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-21 13:57:14 +0900

    [pg] 프레임 조절

--- Project/main.cpp
+++ Project/main.cpp
@@ -11,6 +11,12 @@ enum App {
 void main(void)
 {
 	RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "Project");
+	window.setFramerateLimit(60);	// 초당 프레임 조절(컴퓨터 성능과 상관없이 일정한 속도)
+
+	RectangleShape player;
+	player.setSize(Vector2f(200.f, 200.f));
+	player.setPosition((App::WIDTH - player.getSize().x) / 2, (App::HEIGHT - player.getSize().y) / 2);
+	player.setFillColor(Color::Magenta);
 
 	while (window.isOpen())
 	{
@@ -21,19 +27,14 @@ void main(void)
 				window.close();
 		}
 
-		RectangleShape player;
-		player.setSize(Vector2f(200.f, 200.f));
-		player.setPosition((App::WIDTH - player.getSize().x)/2, (App::HEIGHT - player.getSize().y) / 2);
-		player.setFillColor(Color::Magenta);
-
 		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
 			player.move(10.f, 0.f);
 		if (Keyboard::isKeyPressed(Keyboard::Key::Left))
 			player.move(-10.f, 0.f);
 		if (Keyboard::isKeyPressed(Keyboard::Key::Up))
-			player.move(0.f, 10.f);
-		if (Keyboard::isKeyPressed(Keyboard::Key::Down))
 			player.move(0.f, -10.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Down))
+			player.move(0.f, 10.f);
 
 		window.clear(Color::White);
 		window.draw(player);
